0215d087863e032aa4c1f38886f8017c2cac4a7e
Mateus Luciano <teeusdm@gmail.com>
2021-04-19 19:33:46 -0300

    updated header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import useStyles from './styles';
