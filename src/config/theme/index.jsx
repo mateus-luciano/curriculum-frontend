773a87c7e9bb163934fecd28b501df28853696b9
Mateus Luciano <teeusdm@gmail.com>
2021-04-19 19:19:12 -0300

    created theme

---
+++ src/config/theme/index.jsx
@@ -0,0 +1,30 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ptBR } from '@material-ui/core/locale';
+import 'typeface-roboto';
+
+export default createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: '#f57f45',
+        light: '#e99a72',
+        dark: '#f15b0f'
+      },
+    background: {
+      default: '#f0f0f7',
+    }
+  },
+  typography: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+  },
+  overrides: {
+    MuiButton: {
+        outlined: {
+          borderRadius: 30,
+        }
+      }
+    }
+  },
+  ptBR
+);
