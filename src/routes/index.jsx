5e1cde78bf4d77a9f2fe0d23467b666e3caeff59
Mateus Luciano <teeusdm@gmail.com>
2021-04-19 19:29:01 -0300

    changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Route from './RouteWrapper';
+
 import Home from '../pages/Home';
 import About from '../pages/About';
 
 export default () =>  {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={ Home } />
-                <Route path="/about" component={ About } />
-            </Switch>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={ Home } />
+				<Route path="/about" component={ About } />
+			</Switch>
+		</BrowserRouter>
+	);
 }
