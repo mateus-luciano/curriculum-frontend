074eff94e82235d61173f51965dd39755fd3dbc6
Mateus Luciano <teeusdm@gmail.com>
2021-04-20 20:31:46 -0300

    updated comments

--- src/pages/Comment/index.jsx
+++ src/pages/Comment/index.jsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import NavTabs from '../../components/NavTabs';
 import { 
   Grid, 
   Typography,
-  Box,
-  Tab,
-  Tabs,
-  AppBar } from '@material-ui/core';
-import NavTabs from '../../components/NavTabs';
+  TextField,
+  Button } from '@material-ui/core';
+  import useStyles from './styles';
 
 export default () => {
+  const classes = useStyles();
   const [comments, setComments] = useState();
   
   async function getComments() {
@@ -41,7 +40,43 @@ export default () => {
             </Typography>
           </Grid>
         </Grid>
-      } tableTwo="Table Two" />
+      } tableTwo={
+        <Grid 
+          container 
+          xs={12}
+          direction="column" 
+          align="center" 
+          justify="center"
+        >
+          <Grid item>
+            <form>
+              <TextField 
+                id="name" 
+                label="Nome" 
+                type="text" 
+                variant="outlined" 
+                className={classes.input} 
+              />
+              <br/>
+              <TextField 
+                id="comment" 
+                label="Comentário" 
+                multiline
+                rows={4}
+                variant="outlined" 
+                className={classes.input} 
+              />
+            </form>
+              <Button 
+                color="primary" 
+                variant="contained"
+              >
+                Enviar Comentário
+              </Button>
+          </Grid>
+        </Grid>
+      } 
+    />
     </div>
   );
 }
--- src/pages/Comment/styles.jsx
+++ src/pages/Comment/styles.jsx
@@ -2,9 +2,12 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+    border: '1px solid black',
   },
+  input: {
+    width: '400px',
+    margin: '1em 0'
+  }
 }));
 
 export default useStyles;
