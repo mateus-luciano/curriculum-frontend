0f0c06d186596ccd9ba369a432bb6a9f8fc6a5a7
Mateus Luciano <teeusdm@gmail.com>
2021-04-20 19:36:59 -0300

    updated contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,5 +1,64 @@
+import { 
+  Grid,
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core';
+import useStyles from './styles';
+
 export default () => {
+  const classes = useStyles();
+
   return(
-    <h1>Contact</h1>
+    <div className={classes.root}>
+      <Grid 
+        container 
+        xs={12}
+        direction="column" 
+        align="center" 
+        justify="center"
+      >
+        <Grid item>
+          <form>
+            <TextField 
+              id="name" 
+              label="Nome" 
+              type="text" 
+              variant="outlined" 
+              className={classes.input} 
+            />
+            <br/>
+            <TextField 
+              id="email" 
+              label="E-mail" 
+              type="text" 
+              variant="outlined" 
+              className={classes.input} 
+            />
+            <br/>
+            <TextField 
+              id="phone" 
+              label="Telefone" 
+              type="number" 
+              variant="outlined" 
+              className={classes.input} 
+            />
+            <br/>
+            <TextField 
+              id="message" 
+              label="Mensagem" 
+              multiline
+              rows={4}
+              variant="outlined" 
+              className={classes.input} 
+            />
+          </form>
+            <Button 
+              color="primary" 
+              variant="contained"
+            >Enviar Contato</Button>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
---
+++ src/pages/Contact/styles.jsx
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    border: '1px solid black',
+  },
+  input: {
+    width: '500px',
+    margin: '1em 0'
+  }
+}));
+
+export default useStyles;
