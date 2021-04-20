3e414cad9b713727e92e374fa7554091cd2295fd
Mateus Luciano <teeusdm@gmail.com>
2021-04-19 22:45:57 -0300

    changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,9 @@ import Route from './RouteWrapper';
 
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Comment from '../pages/Comment';
+import Skills from '../pages/Skills';
 
 export default () =>  {
 	return (
@@ -10,6 +13,9 @@ export default () =>  {
 			<Switch>
 				<Route path="/" exact component={ Home } />
 				<Route path="/about" component={ About } />
+				<Route path="/contact" component={ Contact } />
+				<Route path="/comments" component={ Comment } />
+				<Route path="/skills" component={ Skills } />
 			</Switch>
 		</BrowserRouter>
 	);
