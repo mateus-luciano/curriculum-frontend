df04561031acbf33651f2ec05adf2c2c87edcf5f
Mateus Luciano <teeusdm@gmail.com>
2021-04-20 19:14:17 -0300

    add information routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,6 +6,7 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Comment from '../pages/Comment';
 import Skills from '../pages/Skills';
+import Information from '../pages/Information';
 
 export default () =>  {
 	return (
@@ -16,6 +17,7 @@ export default () =>  {
 				<Route path="/contact" component={ Contact } />
 				<Route path="/comments" component={ Comment } />
 				<Route path="/skills" component={ Skills } />
+				<Route path="/informations" component={ Information } />
 			</Switch>
 		</BrowserRouter>
 	);
