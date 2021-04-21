ae88938a10cc9c687459fd73941345af3aba8a32
Mateus Luciano <teeusdm@gmail.com>
2021-04-20 22:41:05 -0300

    updated comment

--- src/pages/Comment/index.jsx
+++ src/pages/Comment/index.jsx
@@ -3,21 +3,12 @@ import { useEffect, useState } from 'react';
 import NavTabs from '../../components/NavTabs';
 import Comment from '../../components/Comment';
 import FormComment from '../../components/FormComment';
-import { 
-  Grid, 
-  TextField,
-  Button,
-  Box } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import useStyles from './styles';
 
 export default () => {
   const classes = useStyles();
   const [comments, setComments] = useState('');
-  // const [name, setName] = useState('');
-  // const [content, setContent] = useState('');
-  // const [saved, setSaved] = useState(false);
-  // const [invalidData, setInvalidData] = useState(false);
-  // const [invalidCredentials, setinvalidCredentials] = useState(false);
   
   async function getComments() {
     const list = []
@@ -31,69 +22,6 @@ export default () => {
     })
     .catch(error => console.log(error))
   }
-
-  // async function handleSubmit(event) {
-  //   event.preventDefault();
-
-  //   setinvalidCredentials(false);
-
-  //   await axios({
-  //     method: 'post',
-  //     url: `http://localhost:3333/comments`,
-  //     data: {
-  //         name: this.name.value,
-  //         content: this.comment.value
-  //     }
-  //   }).then(response => {
-  //     console.log(response.data.data)
-  //     if (response) {
-
-  //     } else {
-  //       setinvalidCredentials(true);
-  //     }
-  //   })
-  //   .catch(error => console.log(error))
-  // }
-
-  // async function onSave(event) {
-  //   event.preventDefault();
-
-  //   setInvalidData(false);
-  //   setSaved(false);
-
-  //   if (!name || !content) {
-  //     setInvalidData(true);
-  //   } else {
-  //     await axios({
-  //       method: 'post',
-  //       url: `http://localhost:3333/comments`,
-  //       data: {
-  //         name: name.value,
-  //         content: content.value
-  //       }
-  //     }).then(response => {
-  //       console.log(response.data.data)
-  //     })
-  //     .catch(error => console.log(error))
-  //       setName('');
-  //       setContent('');
-  //       setSaved(true);
-  //   }
-  // }
-
-  // async function sendComments() {
-  //   await axios({
-  //     method: 'post',
-  //     url: `http://localhost:3333/comments`,
-  //     data: {
-  //         name: this.name.value,
-  //         content: this.comment.value
-  //     }
-  //   }).then(response => {
-  //     console.log(response.data.data)
-  //   })
-  //   .catch(error => console.log(error))
-  // }
 
   useEffect(() => {
     getComments()
@@ -114,52 +42,7 @@ export default () => {
           justify="center"
         >
           <Grid item>
-          {/* {
-            saved 
-            ? 
-              <Box 
-                p={2} 
-                mt={2} 
-                bgcolor="green" 
-                color="primary.contrastText"
-                >
-                Salvo com sucesso
-              </Box>
-            : ''
-          }
-            <form>
-              <TextField 
-                id="name" 
-                label="Nome" 
-                type="text" 
-                variant="outlined" 
-                className={classes.input} 
-                value={name}
-                onChange={e => setName(e.target.value)}
-                error={invalidData}
-              />
-              <br />
-              <TextField 
-                id="comment" 
-                label="Comentário" 
-                multiline
-                rows={4}
-                variant="outlined" 
-                className={classes.input}
-                value={content}
-                onChange={e => setContent(e.target.value)}
-                error={invalidData}
-              /> 
-            </form>
-              <Button 
-                type="submit"
-                color="primary" 
-                variant="contained"
-                onClick={onSave}
-              >
-                Enviar Comentário
-              </Button> */}
-              <FormComment />
+            <FormComment />
           </Grid>
         </Grid>
       } 
