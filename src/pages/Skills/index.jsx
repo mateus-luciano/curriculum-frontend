43100cdf5b406c5bf73091b2dd8494249cc5c276
Mateus Luciano <teeusdm@gmail.com>
2021-04-21 09:36:28 -0300

    updated skils

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,5 +1,39 @@
+import Skills from '../../components/Skills';
+import { Grid } from '@material-ui/core';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
 export default () => {
   return(
-    <h1>Skills</h1>
+    <div>
+      <Grid container xs={12}>
+        <Grid item>
+          <Skills 
+            title="FRONTEND" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]}
+          />
+          <Skills 
+            title="BACKEND" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]} 
+          />
+          <Skills 
+            title="DATABASE" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]} 
+          />
+          <Skills 
+            title="SERVER" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]} 
+          />
+          <Skills 
+            title="VERSIONING" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]} 
+          />
+          <Skills 
+            title="PACKAGE MANAGER" 
+            icons={[ <MailIcon />, <MailIcon />, <MailIcon />, <AccountCircleIcon /> ]} 
+          />
+        </Grid> 
+      </Grid>
+    </div>
   );
 }
